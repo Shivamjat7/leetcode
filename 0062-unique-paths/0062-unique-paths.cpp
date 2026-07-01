97df99eaa481363bc2fd2310d5febf34eb08a2c1
Shivam Jat <shivamjat531@gmail.com>
2026-07-01 23:47:48 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.7%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int>prev(n,0);
+        prev[0]=1;
+        for(int i=0;i<m;i++){
+            vector<int>curr(n,0);
+            for(int j=0;j<n;j++){
+                curr[j]+=prev[j];
+                if(j>0)curr[j]+=curr[j-1];
+            }
+            prev=curr;
+        }
+        return prev[n-1];
+    }
+};
