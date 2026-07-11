0dd081d7de83c625d6cc55f270203b87eb20ae9d
Shivam Jat <shivamjat531@gmail.com>
2026-07-11 12:33:37 +0530

    Time: 84 ms (10%), Space: 21.1 MB (12.45%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(m+1, vector<int>(m+1, 0)));
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        for (int i = n-2; i>=0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+
+                    int mx = -1e9;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            if(j1+dj1<0 ||j2+dj2<0 ||j1+dj1>=m ||j2+dj2>=m)continue;
+                            int value = 0;
+                            if (j1 == j2) {
+                                value = grid[i][j1];
+                            } else
+                                value = grid[i][j1] + grid[i][j2];
+                            value += dp[i + 1] [j1 + dj1][j2 + dj2];
+                            mx = max(mx, value);
+                        }
+                    }
+                    dp[i][j1][j2]=mx;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+};
